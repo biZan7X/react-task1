0dd2e22cdfa0cdc34294a92eb6c206aa548cc03e
Biswanath Tewari <biswanath@geekyants.com>
2021-07-16 14:05:49 +0530

    feat - welcome message

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>"Welcome in Geekyants"</h1>
+  ,
   document.getElementById('root')
 );
 
